1ca0ecd271d300193efd675dd463aef4ab2fb891
leandrochs <leandrochagask@gmail.com>
2024-03-05 10:47:08 -0300

    initial commit

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+
+type Props = {
+    children: ReactNode;
+    open: boolean;
+    onClose: () => void;
+}
+
+export const Sidebar = ({ open, onClose, children }: Props) => {
+    return (
+        <section
+            className={`fixed  left-0 top-0 bottom-0 text-white 
+            ${open ? 'w-screen bg-gray-600/75' : 'w-0'}`}
+        >
+
+        </section>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,10 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'gpt-gray': '#343541',
+        'gpt-lightgray': '#40414F'
+      }
     },
   },
   plugins: [],
