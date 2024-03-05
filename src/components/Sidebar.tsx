d5eb91bb57582d2dd093cf4e369f32a632a6f25e
leandrochs <leandrochagask@gmail.com>
2024-03-05 11:44:33 -0300

    feat: adiciona novos campos para sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import IconClose from "./icons/IconClose";
+import IconAdd from "./icons/IconAdd";
 
 type Props = {
   children: ReactNode;
@@ -17,7 +18,19 @@ export const Sidebar = ({ open, onClose, children }: Props) => {
         className={`transition-all duration-200 flex h-screen 
                     ${open ? "ml-0" : "-ml-96"} md:ml-0`}
       >
-        <div className="flex flex-col w-64 p-2 bg-gray-900">barra</div>
+        <div className="flex flex-col w-64 p-2 bg-gray-900">
+            <div className="flex items-center p-3 rounded-md
+            text-sm cursor-pointer border border-white/20 hover:bg-gray-500/20">
+                <IconAdd width={16} height={16} className="mr-3" />
+                Nova Conversa
+            </div>
+            <nav className="flex-1 pt-2 overflow-y-auto">
+                {children}
+            </nav>
+            <div className="border-t border-gray-700 pt-2">
+
+            </div>
+        </div>
         <div
           onClick={onClose}
           className="flex justify-center items-center w-10 h-10 cursor-pointer md:hidden"
