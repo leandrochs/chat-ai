ef50b3d4e7d65da4a3d6edd5d3739abc16f0c717
leandrochs <leandrochagask@gmail.com>
2024-03-05 14:45:50 -0300

    feat: adiciona header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,36 @@
 "use client";
 
+import { Header } from "@/components/Header";
 import { Sidebar } from "@/components/Sidebar";
 import { useState } from "react";
 
 const Page = () => {
   const [sidebarOpened, setSidebarOpened] = useState(false);
-  const closeSidebar = () => {
-    setSidebarOpened(false)
-  };
 
-  const handleClearConversations = () => {
-    
-  }
+  const openSidebar = () => setSidebarOpened(true)
+  const closeSidebar = () => setSidebarOpened(false);
+
+  const handleClearConversations = () => {};
+
+  const handleNewChat = () => {};
 
   return (
-    <main className="flex min-h-screen bg-gpt-gray">
+    <main className="flex min-h-screen bg-gpt-gray text-white">
       <Sidebar
         open={sidebarOpened}
         onClose={closeSidebar}
         onClear={handleClearConversations}
-        children={undefined}
-      ></Sidebar>
+        onNewChat={handleNewChat}
+      >
+        <div>...</div>
+      </Sidebar>
       <section className="flex flex-col w-full">
-        <button onClick={() => setSidebarOpened(true)}>Abrir Sidebar</button>
+        <Header 
+          openSidebarClick={openSidebar}
+          title={`bla bal bal`}
+          newChatClick={handleNewChat}
+        />
+        
       </section>
     </main>
   );
