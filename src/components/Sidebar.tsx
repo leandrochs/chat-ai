3ecd9e66cdd2162d61389943d27fbd4cc8173e48
leandrochs <leandrochagask@gmail.com>
2024-03-05 10:50:30 -0300

    feat: limita tamanho do sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -10,7 +10,7 @@ export const Sidebar = ({ open, onClose, children }: Props) => {
     return (
         <section
             className={`fixed  left-0 top-0 bottom-0 text-white 
-            ${open ? 'w-screen bg-gray-600/75' : 'w-0'}`}
+            ${open ? 'w-screen bg-gray-600/75' : 'w-0'} md:w-64 md:static`}
         >
 
         </section>
