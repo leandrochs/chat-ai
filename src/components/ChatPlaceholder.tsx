1117d818edd661a04788ec9202a5ce8ebc56d633
leandrochs <leandrochagask@gmail.com>
2024-03-07 23:30:42 -0300

    emprove: Atualiza titulo inicial da area de chat

--- src/components/ChatPlaceholder.tsx
+++ src/components/ChatPlaceholder.tsx
@@ -6,7 +6,7 @@ import IconLinkedin from "./icons/IconLinkedin";
 export const ChatPlaceholder = () => {
   return (
     <div className="m-5">
-      <h3 className="text-4xl font-bold text-center my-8">CHAT AI</h3>
+      <h3 className="text-4xl font-bold text-center my-8">CHAT OPENAI</h3>
       <div className="flex flex-col justify-center md:flex-row gap-5 m-auto mb-8 md:max-w-4xl">
         <div>
           <div className="flex justify-center items-center text-lg mb-3">
