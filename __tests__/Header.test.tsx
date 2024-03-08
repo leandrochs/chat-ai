3886f0c3244b4cc11504764d81b89df66aca53c9
leandrochs <leandrochagask@gmail.com>
2024-03-07 22:30:55 -0300

    style: lint format

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -1,25 +1,41 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import { Header } from '@/components/Header';
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Header } from "@/components/Header";
 
-describe('Header', () => {
-
-  it('renderiza o título corretamente', () => {
-    render(<Header openSidebarClick={() => {}} title="Conversa" newChatClick={() => {}} />);
-    expect(screen.getByText('Conversa')).toBeInTheDocument();
+describe("Header", () => {
+  it("renderiza o título corretamente", () => {
+    render(
+      <Header
+        openSidebarClick={() => {}}
+        title="Conversa"
+        newChatClick={() => {}}
+      />
+    );
+    expect(screen.getByText("Conversa")).toBeInTheDocument();
   });
 
-  it('chama a função openSidebarClick quando o ícone de menu é clicado', () => {
+  it("chama a função openSidebarClick quando o ícone de menu é clicado", () => {
     const openSidebarClickMock = jest.fn();
-    render(<Header openSidebarClick={openSidebarClickMock} title="Conversa" newChatClick={() => {}} />);
-    fireEvent.click(screen.getByTestId('icon-menu'));
+    render(
+      <Header
+        openSidebarClick={openSidebarClickMock}
+        title="Conversa"
+        newChatClick={() => {}}
+      />
+    );
+    fireEvent.click(screen.getByTestId("icon-menu"));
     expect(openSidebarClickMock).toHaveBeenCalled();
   });
 
-  it('chama a função newChatClick quando o ícone de adicionar é clicado', () => {
+  it("chama a função newChatClick quando o ícone de adicionar é clicado", () => {
     const newChatClickMock = jest.fn();
-    render(<Header openSidebarClick={() => {}} title="Conversa" newChatClick={newChatClickMock} />);
-    fireEvent.click(screen.getByTestId('icon-add'));
+    render(
+      <Header
+        openSidebarClick={() => {}}
+        title="Conversa"
+        newChatClick={newChatClickMock}
+      />
+    );
+    fireEvent.click(screen.getByTestId("icon-add"));
     expect(newChatClickMock).toHaveBeenCalled();
   });
-
 });
