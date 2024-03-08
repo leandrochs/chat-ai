6628a07436cc21fdfecfbbec49eb9e40db5ee8a5
leandrochs <leandrochagask@gmail.com>
2024-03-07 22:28:55 -0300

    test: chama a funcao openSidebarClick quando o icone de menu e clicado

--- __tests__/Header.test.tsx
+++ __tests__/Header.test.tsx
@@ -1,12 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Header } from '@/components/Header';
 
 describe('Header', () => {
+
   it('renderiza o título corretamente', () => {
     render(<Header openSidebarClick={() => {}} title="Conversa" newChatClick={() => {}} />);
     expect(screen.getByText('Conversa')).toBeInTheDocument();
   });
+
+  it('chama a função openSidebarClick quando o ícone de menu é clicado', () => {
+    const openSidebarClickMock = jest.fn();
+    render(<Header openSidebarClick={openSidebarClickMock} title="Conversa" newChatClick={() => {}} />);
+    fireEvent.click(screen.getByTestId('icon-menu'));
+    expect(openSidebarClickMock).toHaveBeenCalled();
+  });
+
 
 
 });
