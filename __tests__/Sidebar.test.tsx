03be51e73102b8ee256559e5577d23bd1bff7bec
leandrochs <leandrochagask@gmail.com>
2024-03-07 22:47:45 -0300

    test: dispara onClose quando o icone de fechar e clicado

--- __tests__/Sidebar.test.tsx
+++ __tests__/Sidebar.test.tsx
@@ -1,45 +1,81 @@
-import { render, screen } from '@testing-library/react';
-import { Sidebar } from '@/components/Sidebar';
+import { fireEvent, render, screen } from "@testing-library/react";
+import { Sidebar } from "@/components/Sidebar";
 
-describe('Sidebar', () => {
-  it('abre e fecha de acordo com a prop open', () => {
+describe("Sidebar", () => {
+  it("abre e fecha de acordo com a prop open", () => {
     const { rerender } = render(
-      <Sidebar open={false} onClose={() => {}} onClear={() => {}} onNewChat={() => {}}>
+      <Sidebar
+        open={false}
+        onClose={() => {}}
+        onClear={() => {}}
+        onNewChat={() => {}}
+      >
         <div>Conteúdo da Sidebar</div>
       </Sidebar>
     );
-    let sidebarSection = screen.getByTestId('sidebar-section');
-    expect(sidebarSection).toHaveClass('w-0');
+    let sidebarSection = screen.getByTestId("sidebar-section");
+    expect(sidebarSection).toHaveClass("w-0");
 
     rerender(
-      <Sidebar open={true} onClose={() => {}} onClear={() => {}} onNewChat={() => {}}>
+      <Sidebar
+        open={true}
+        onClose={() => {}}
+        onClear={() => {}}
+        onNewChat={() => {}}
+      >
         <div>Conteúdo da Sidebar</div>
       </Sidebar>
     );
 
-    sidebarSection = screen.getByTestId('sidebar-section');
-    expect(sidebarSection).not.toHaveClass('w-0');
+    sidebarSection = screen.getByTestId("sidebar-section");
+    expect(sidebarSection).not.toHaveClass("w-0");
   });
 
-  it('dispara onNewChat quando o botão Nova Conversa é clicado', () => {
+  it("dispara onNewChat quando o botão Nova Conversa é clicado", () => {
     const onNewChatMock = jest.fn();
     render(
-      <Sidebar open={true} onClose={() => {}} onClear={() => {}} onNewChat={onNewChatMock}>
+      <Sidebar
+        open={true}
+        onClose={() => {}}
+        onClear={() => {}}
+        onNewChat={onNewChatMock}
+      >
         <div></div>
       </Sidebar>
     );
-    screen.getByText('Nova Conversa').click();
+    screen.getByText("Nova Conversa").click();
     expect(onNewChatMock).toHaveBeenCalled();
   });
 
-  it('dispara onClear quando o botão Limpar todas as conversas é clicado', () => {
+  it("dispara onClear quando o botão Limpar todas as conversas é clicado", () => {
     const onClearMock = jest.fn();
     render(
-      <Sidebar open={true} onClose={() => {}} onClear={onClearMock} onNewChat={() => {}}>
+      <Sidebar
+        open={true}
+        onClose={() => {}}
+        onClear={onClearMock}
+        onNewChat={() => {}}
+      >
         <div></div>
       </Sidebar>
     );
-    screen.getByText('Limpar todas as conversas').click();
+    screen.getByText("Limpar todas as conversas").click();
     expect(onClearMock).toHaveBeenCalled();
+  });
+
+  it("dispara onClose quando o ícone de fechar é clicado", () => {
+    const onCloseMock = jest.fn();
+    render(
+      <Sidebar
+        open={true}
+        onClose={onCloseMock}
+        onClear={() => {}}
+        onNewChat={() => {}}
+      >
+        <div></div>
+      </Sidebar>
+    );
+    fireEvent.click(screen.getByTestId("icon-close"));
+    expect(onCloseMock).toHaveBeenCalled();
   });
 });
